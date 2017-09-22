d770b77273c554d49932ab89fc38935fe66aad01
tmccarten <thomasmccarten@hotmail.com>
2017-09-22 13:41:04 +0100

    add nav dropdown and button animations

---
+++ assets/includes/head.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+
+    <meta charset="UTF-8">
+
+    <title><?php echo $title ?></title>
+
+    <meta name="description" content="<?php echo $description ?>">
+    <meta name="keywords" content="<?php echo $keywords ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="icon" href="favicon.svg">
+
+    <link rel="stylesheet" href="<?php echo $index?>/assets/css/normalize.css">
+    <link rel="stylesheet" href="<?php echo $index?>/assets/css/style.css">
+
+  </head>
--- index.php
+++ index.php
@@ -5,13 +5,12 @@
 <?php $description = "Thomas McCarten is a Front End Developer/Designer based in Medway, Kent."; ?>
 <?php $keywords = "Thomas McCarten,Front-End,Design,Web Development,Logo Design,Illustrations"; ?>
 
+<?php include 'assets/includes/head.php';?>
 
   <body>
-
     <?php include 'assets/includes/header.php';?>
 
-
-  <script src="<?php echo $index?>/assets/js/script.js"></script>
+    <script src="<?php echo $index?>/assets/js/script.js"></script>
   </body>
 
 </html>
