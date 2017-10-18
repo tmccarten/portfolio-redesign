bc12fa7bc42bc9d40870b0f450879b24a760f51d
tmccarten <thomasmccarten@hotmail.com>
2017-10-18 11:12:28 +0100

    add droid sans webfont

--- assets/includes/head.php
+++ assets/includes/head.php
@@ -10,9 +10,7 @@
     <meta name="keywords" content="<?php echo $keywords ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
 
-    <link rel="icon" href="favicon.svg">
-
-    <link href="https://fonts.googleapis.com/css?family=Roboto:400,500" rel="stylesheet">
+    <link rel="icon" href="favicon.svg">    
 
     <link rel="stylesheet" href="<?php echo $index?>/assets/css/normalize.css">
     <link rel="stylesheet" href="<?php echo $index?>/assets/css/style.css">
