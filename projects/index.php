cdd12d05144a25fd3c213d5306d216bbdf9c7ce6
tmccarten <thomasmccarten@hotmail.com>
2017-10-20 14:43:20 +0100

    project layout

--- projects/index.php
+++ projects/index.php
@@ -18,16 +18,17 @@
           <div class="content__inner">
 
             <section class="project">
-              <div class="project__wrapper">
-                <h3 class="project__title">Living Lines: Birley Archive Project</h3>
+              <h3 class="project__title">Living Lines: Birley Archive Project</h3>
+              <div class="project__imgbtn-wrapper">
                 <img class="project__img" src="<?php echo $index?>/assets/img/project_birley.png" width="" height="" alt="Birley Archive Project Screenshot">
                 <button class="project__btn-view">View site</button>
-                <h4 class="project__header">Tools</h4>
-                <p class="project__text">HTML5, CSS3, Sass, jQuery, JavaScript, PHP</p>
               </div>
-              <div class="project__wrapper project__wrapper--right">
+              <div class="project__info">
+                <h3 class="project__title project__title--no-mob">Living Lines: Birley Archive Project</h3>
                 <h4 class="project__header">Overview</h4>
                 <p class="project__text"><span class="project__text--intro">Living Lines is a project documenting the changes that have occurred around the Hulme area of Manchester since the 1800s, with a particular focus on Birley, the site of Manchester Metropolitan University's newest campus.</span>A challenging project, as the sole web developer I was responsible for the design and development of the website, collating various media and content types to produce an accessible and responsive solution. The client had a concept based around illustrated (illustrations and handwritten titles by <a href="https://katedunstone.co.uk/">Kate Dunstone</a>), interactive maps to demonstrate the different 'layers' of history at the Birley site. Creating these and ensuring a sound mobile user experience was challenging but something from which I learned a great deal.</p>
+                <h4 class="project__header">Tools</h4>
+                <p class="project__text">HTML5, CSS3, Sass, jQuery, JavaScript, PHP</p>
               </div>
             </section>
 
