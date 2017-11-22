9f06578442aedadf03b7191d7f59cf15af1df201
tmccarten <thomasmccarten@hotmail.com>
2017-11-22 17:05:25 +0000

    main size issues and about images

--- about/index.php
+++ about/index.php
@@ -20,11 +20,13 @@
 
             <section class="about">
               <div class="about__img-wrapper">
-                <img class="about__img" src="<?php echo $index?>/assets/img/about/dock.png" width="300" height="300" alt="Portrait photo of Tom McCarten">
-                <img class="about__img" src="<?php echo $index?>/assets/img/about/pub.png" width="300" height="300" alt="Portrait photo of Tom McCarten">
+                <img class="about__img-aside" src="<?php echo $index?>/assets/img/about/tom-and-carly.png" width="300" height="300" alt="Tom McCarten and girlfriend both wearing shades on sunny day.">
+                <img class="about__img-aside" src="<?php echo $index?>/assets/img/about/portrait-beach.png" width="300" height="300" alt="Portrait photo of Tom McCarten">
+
               </div>
               <div class="about__info">
                 <h3 class="content__heading">Hello</h3>
+                <img class="about__img" src="<?php echo $index?>/assets/img/about/pub.png" width="625" height="417" alt="Tom McCarten sat in a pub with lots of beer mats on wall above.">
                 <p class="about__text">I graduated from Manchester Metropolitan University in July 2017 with First-class Honours and the Programme Leaders Prize in BSc Web Development.</p>
                 <p class="about__text">I subsequently moved down south where I now seek front-end roles (or similar) in Medway and the surrounding areas.</p>
                 <p class="about__text">Fluent in CSS/Sass with a strong working knowledge of JavaScript and PHP, I consider myself equal parts designer/developer.</p>
