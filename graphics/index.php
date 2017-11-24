3878c0f95945fc51f4fba332d44e36e4c888a3b1
tmccarten <thomasmccarten@hotmail.com>
2017-11-24 16:12:03 +0000

    graphics images make links

--- graphics/index.php
+++ graphics/index.php
@@ -23,8 +23,10 @@
 
             <section class="work">
               <h3 class="content__heading content__heading--no-desktop">Personas</h3>
-              <div class="work__imgbtn-wrapper work__imgbtn-wrapper--graphics">
-                <img class="work__img" src="<?php echo $index?>/assets/img/graphics/personas.png" width="575" height="404" alt="Illustrated colourful depictions of boy and girl portraits">
+              <div class="work__wrap">
+                <a href="<?php echo $index?>/assets/img/graphics/personas.png">
+                  <img class="work__img" src="<?php echo $index?>/assets/img/graphics/personas.png" width="575" height="404" alt="Illustrated colourful depictions of boy and girl portraits">
+                </a>
               </div>
               <div class="work__info">
                 <h3 class="content__heading content__heading--no-mob">Personas</h3>
@@ -40,8 +42,10 @@
 
             <section class="work">
               <h3 class="content__heading content__heading--no-desktop">Logos</h3>
-              <div class="work__imgbtn-wrapper work__imgbtn-wrapper--graphics">
-                <img class="work__img" src="<?php echo $index?>/assets/img/graphics/logos.png" width="575" height="334" alt="Collection of logos">
+              <div class="work__wrap">
+                <a href="<?php echo $index?>/assets/img/graphics/logos.png">
+                  <img class="work__img" src="<?php echo $index?>/assets/img/graphics/logos.png" width="575" height="334" alt="Collection of logos">
+                </a>
               </div>
               <div class="work__info">
                 <h3 class="content__heading content__heading--no-mob">Logos</h3>
@@ -57,15 +61,36 @@
 
             <section class="work">
               <h3 class="content__heading content__heading--no-desktop">Posters</h3>
-              <div class="work__imgbtn-wrapper work__imgbtn-wrapper--graphics">
-                <img class="work__img" src="<?php echo $index?>/assets/img/graphics/other_docposter.jpg" width="575" height="813" alt="Documentary poster for Manchester IRA Bomb with a postbox broken up into pieces.">
-                <img class="work__img" src="<?php echo $index?>/assets/img/graphics/other_productposter.jpg" width="575" height="407" alt="Poster advertising a tour of Manchester IRA Bomb location with postbox and shattered glass appearance.">
+              <div class="work__wrap">
+                <a href="<?php echo $index?>/assets/img/graphics/other_docposter.jpg">
+                  <img class="work__img" src="<?php echo $index?>/assets/img/graphics/other_docposter.jpg" width="575" height="813" alt="Documentary poster for Manchester IRA Bomb with a postbox broken up into pieces.">
+                </a>
+                <a href="<?php echo $index?>/assets/img/graphics/other_productposter.jpg">
+                  <img class="work__img" src="<?php echo $index?>/assets/img/graphics/other_productposter.jpg" width="575" height="407" alt="Poster advertising a tour of Manchester IRA Bomb location with postbox and shattered glass appearance.">
+                </a>
               </div>
               <div class="work__info">
                 <h3 class="content__heading content__heading--no-mob">Posters</h3>
                 <h4 class="work__header">Overview</h4>
                 <p class="work__text">Posters made for the Creative Digital Media Production unit of BSc Web Development at university. The brief involved the production of a short film based on a subject of our choosing. The first poster advertises the film whilst the second is based around the idea of an associated 'product'. Both posters were to be inspired by a chosen art movement. I decided on cubism, although the link is rather tenuous.</p>
+              </div>
+            </section>
 
+          </div>
+
+          <div class="content__item content__item--project">
+
+            <section class="work">
+              <h3 class="content__heading content__heading--no-desktop">Ilustration</h3>
+              <div class="work__wrap">
+                <a href="<?php echo $index?>/assets/img/graphics/path.png">
+                  <img class="work__img" src="<?php echo $index?>/assets/img/graphics/path.png" width="574" height="900" alt="Illustration of path leading into distance with mountains on horizon and tall trees either side. A male with a backpack looks ahead.">
+                </a>
+              </div>
+              <div class="work__info">
+                <h3 class="content__heading content__heading--no-mob">Illustration</h3>
+                <h4 class="work__header">Overview</h4>
+                <p class="work__text">This was a perspective experiment in Illustrator that I got slightly carried away with. Not really sure what to call it. Man stands on path with mountains in distance and strange trees either side.
               </div>
             </section>
 
